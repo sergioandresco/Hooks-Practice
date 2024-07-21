51e39c2306ef23f3dd9991724e35e539091ac321
sergio-cobos <sergio.cobos@tuboleta.com>
2024-07-21 18:50:21 -0500

    fix styles and disable click for particles

--- src/components/background/particles/index.jsx
+++ src/components/background/particles/index.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-
 import { loadFull } from "tsparticles";
+
 export default function Particle() {
 
     const [init, setInit] = useState(false);
 
     useEffect(() => {
         initParticlesEngine(async (engine) => {
-        await loadFull(engine);
+            await loadFull(engine);
         }).then(() => {
-        setInit(true);
+            setInit(true);
         });
     }, []);
 
     const particlesLoaded = (container) => {
+        console.log(container);
     };
 
     return (
@@ -24,69 +25,71 @@ export default function Particle() {
                 id="tsparticles"
                 particlesLoaded={particlesLoaded}
                 style={{
-                    zIndex: 1,
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    zIndex: -1,
                 }}
                 options={{
                     fpsLimit: 120,
                     interactivity: {
-                    events: {
-                        onClick: {
-                        enable: true,
-                        mode: "push",
+                        events: {
+                            onClick: {
+                                enable: false,
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
                         },
-                        onHover: {
-                        enable: true,
-                        mode: "repulse",
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
                         },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                        quantity: 4,
-                        },
-                        repulse: {
-                        distance: 200,
-                        duration: 0.4,
-                        },
-                    },
                     },
                     particles: {
-                    color: {
-                        value: "#0ed9ff",
-                    },
-                    links: {
-                        color: "#0ed9ff",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                        default: "bounce",
+                        color: {
+                            value: "#0ed9ff",
                         },
-                        random: false,
-                        speed: 1.2,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                        enable: true,
-                        area: 800,
+                        links: {
+                            color: "#0ed9ff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
                         },
-                        value: 220,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 5 },
-                    },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                            default: "bounce",
+                            },
+                            random: false,
+                            speed: 1.2,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                            enable: true,
+                            area: 800,
+                            },
+                            value: 220,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 5 },
+                        },
                     },
                     detectRetina: true,
                 }}
